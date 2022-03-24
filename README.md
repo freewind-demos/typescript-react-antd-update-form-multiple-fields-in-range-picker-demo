fb81b35327c560de7cdb2bb7de801a3eba7070f8
Freewind <nowindlee@gmail.com>
2022-03-24 17:08:51 +0800

    typescript-react-antd-update-form-multiple-fields-in-range-picker-demo

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-TypeScript React AntDesign Demo
-=================================
+TypeScript React AntDesign Update Form Multiple Fields in RangePicker Demo
+===========================================
+
+如何在一个组件中更新form中多个field?
+
+比如:
+
+```
+type FormData = {
+  startDate?: string,
+  closeDate?: string,
+}
+```
+
+我现在使用了一个辅助的`formData`，虽然可以达到目的，但是有没有更简单的办法？
 
 ```
 npm install
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,6 +1,46 @@
-import React from 'react'
-import {Alert} from 'antd';
+import React, {useEffect, useState} from 'react'
+import {DatePicker, Form} from 'antd';
+import {useForm} from 'antd/lib/form/Form';
+import moment from 'moment';
+
+type FormData = {
+  startDate?: string,
+  closeDate?: string,
+}
 
 export default function Hello() {
-  return <Alert message={'Hello'}/>
+
+  const [formData, setFormData] = useState<FormData>({
+    startDate: '2020-01-02',
+    closeDate: '2020-01-04'
+  })
+
+  const [form] = useForm<FormData>();
+
+  useEffect(() => {
+    console.log("### formData", formData);
+    form.setFieldsValue(formData)
+  }, [formData])
+
+  return <Form form={form} onValuesChange={(_, values) => setFormData(values)}>
+    <Form.Item>
+      <DatePicker.RangePicker
+        value={[
+          formData?.startDate ? moment(formData.startDate, 'YYYY-MM-DD') : null,
+          formData?.closeDate ? moment(formData.closeDate, 'YYYY-MM-DD') : null
+        ]}
+        onChange={(dates) => {
+          setFormData(d => ({
+            ...d,
+            ...({
+              startDate: dates?.[0]?.format('YYYY-MM-DD'),
+              closeDate: dates?.[1]?.format('YYYY-MM-DD')
+            })
+          }))
+        }}
+      />
+    </Form.Item>
+    <hr/>
+    <pre>{JSON.stringify(formData, null, 4)}</pre>
+  </Form>
 };
